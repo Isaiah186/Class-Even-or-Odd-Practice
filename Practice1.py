cdd5d99ec5b278a930449f3a8cd7cd35647171be
Isaiah186 <91351729+Isaiah186@users.noreply.github.com>
2021-11-06 17:38:48 -0500

    Add files via upload

---
+++ Practice1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+class EvenorOdd:
+    def __init__(self):
+        pass
+    
+    def number(self):
+        num = int(input("Pick a Number"))
+        
+        if num%2==0:
+            print("This number is even.")
+            
+        if num%2!=0:
+            print("This number is odd.")
+            
+
+
+# In[10]:
+
+
+p1 = EvenorOdd()
+
+
+# In[13]:
+
+
+p1.number()
+
+
+# p1.num()
+
+# In[2]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
